7c3492e9575c60512edb21f34748949b887279af
Marc Jennings <marc.jenn.ings@gmail.com>
2017-03-16 17:43:33 +0000

    Added auto hotspot capability (untested)@ @

--- setup.sh
+++ setup.sh
@@ -12,7 +12,19 @@ cp -R web/* /var/www/html
 # Grant sudo access to www-data
 cat  > /etc/sudoers.d/020_www-data-nopasswd << EOF
 www-data ALL=(ALL) NOPASSWD: /sbin/iwconfig, /sbin/iwlist, /bin/grep
-
 EOF
 
+# Set up an access point for initial configuration
+apt-get install hostapd -y
+apt-get install dnsmasq -y
 
+service hostapd stop
+service dnsmasq stop
+
+cp includedFiles/hostapd.conf /etc/hostapd/hostapd.conf
+cp includedFiles/interfaces /etc/network/interfaces
+cp includedFiles/autohotspot /usr/bin/autohotspot
+chmod +x /usr/bin/autohotspot
+
+cp includedFiles/autpohotspot.service /etc/systemd/system/autohotspot.service
+service autohotspot.service start
