611cebe188da836c6177ff064407c5515fd2c262
MarcJenningsUK <marc.jenning.s@gmail.com>
2017-03-17 11:36:52 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-#apt-get update
+apt-get update
 
-## Install apache and php
-#apt-get install apache2 -y
-#apt-get install php5 libapache2-mod-php5 -y
+# Install apache and php
+apt-get install apache2 -y
+apt-get install php5 libapache2-mod-php5 -y
 
-## Copy the web folder content to the new web root
-#rm /var/www/html/index.html
-#cp -R web/* /var/www/html
+# Copy the web folder content to the new web root
+rm /var/www/html/index.html
+cp -R web/* /var/www/html
 
-## Grant sudo access to www-data
-#cat  > /etc/sudoers.d/020_www-data-nopasswd << EOF
-#www-data ALL=(ALL) NOPASSWD: /sbin/iwconfig, /sbin/iwlist, /bin/grep
-#EOF
+# Grant sudo access to www-data
+cat  > /etc/sudoers.d/020_www-data-nopasswd << EOF
+www-data ALL=(ALL) NOPASSWD: /sbin/iwconfig, /sbin/iwlist, /bin/grep
+EOF
 
-## Set up an access point for initial configuration
-#apt-get install hostapd -y
-#apt-get install dnsmasq -y
+# Set up an access point for initial configuration
+apt-get install hostapd -y
+apt-get install dnsmasq -y
 
 service hostapd stop
 service dnsmasq stop
