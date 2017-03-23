ceffcefe54822f44ec20c4457eb42e01b137401b
Marc Jennings <marc.jennings@gmail.com>
2017-03-23 10:57:16 +0000

    Added output library for OLED

--- python/OledOutput.py
+++ python/OledOutput.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
+
+import time
+import RPi.GPIO as GPIO
 import Adafruit_SSD1306
 from PIL import Image
 from PIL import ImageFont
@@ -5,11 +9,12 @@ from PIL import ImageDraw
 
 temp = "1"
 humid = "2"
-image = "branding.png"
+imageFile = "branding.png"
 ipAddress = "3"
-copyrightText = "©2017 Marc Jennings"
+copyrightText1 = "©2017 Marc"
+copyrightText2 = "Jennings"
 
-tempSuffix = "°C"
+tempSuffix = " deg C"
 humidSuffix = "%"
 
 displayMode = 0
@@ -17,16 +22,18 @@ displayMode = 0
 # set up the oled.
 RST = 4
 disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, i2c_address=0x3d)
+
 disp.begin()
+
 width = disp.width
 height = disp.height
 disp.clear()
 disp.display()
+
 image = Image.new('1', (width, height))
 font = ImageFont.truetype('segoe-wp.ttf', 24)
 bigfont = ImageFont.truetype('segoe-wp.ttf', 48)
 draw = ImageDraw.Draw(image)
-
 
 # MODES
 # 0 - temp and humidity
@@ -39,52 +46,60 @@ draw = ImageDraw.Draw(image)
 validNodeCount = 6
 
 def Display():
+	global disp
+	global draw
+
 	global temp
 	global humid
 	global image
 	global ipAddress
-	global image
+	global imageFile
+	global copyrightText1
+	global copyrightText2
 	
 	# display 2 lines
 	if(displayMode == 0):
-				draw.rectangle((0,0,width,height), outline=0, fill=0)
-				draw.text((0, 0), temp + tempSuffix, font=font, fill=255)
-				draw.text((0, 32), humid + humidSuffix, font=font, fill=255)
-				disp.image(image)
+		draw.rectangle((0,0,width,height), outline=0, fill=0)
+		draw.text((0, 0), temp + tempSuffix, font=font, fill=255)
+		draw.text((0, 32), humid + humidSuffix, font=font, fill=255)
+		disp.image(image)
 	
 	# display just temperature
 	elif(displayMode == 1):
-				draw.rectangle((0,0,width,height), outline=0, fill=0)
-				draw.text((0, 0), temp + tempSuffix, font=bigfont, fill=255)
-				disp.image(image)
+		draw.rectangle((0,0,width,height), outline=0, fill=0)
+		draw.text((0, 0), temp + tempSuffix, font=bigfont, fill=255)
+		disp.image(image)
 	
 	# display just humidity
 	elif(displayMode == 2):
-				draw.rectangle((0,0,width,height), outline=0, fill=0)
-				draw.text((0, 0), humid + humidSuffix, font=bigfont, fill=255)
-				disp.image(image)
+		draw.rectangle((0,0,width,height), outline=0, fill=0)
+		draw.text((0, 0), humid + humidSuffix, font=bigfont, fill=255)
+		disp.image(image)
 	
 	# display ip address
 	elif(displayMode == 3):
-				draw.rectangle((0,0,width,height), outline=0, fill=0)
-				draw.text((0, 0), ipAddress, font=bigfont, fill=255)
-				disp.image(image)
+		draw.rectangle((0,0,width,height), outline=0, fill=0)
+		draw.text((0, 0), "current IP", font=font, fill=255)
+		draw.text((0, 32), ipAddress, font=font, fill=255)
+		disp.image(image)
 	
 	# display branding image
 	elif(displayMode == 4):
-				draw.rectangle((0,0,width,height), outline=0, fill=0)
-				disp.image(brandimage)
+		brandimage = Image.open(imageFile).convert('1')
+		draw.rectangle((0,0,width,height), outline=0, fill=0)
+		disp.image(brandimage)
 	
 	# display copyright notice
 	elif(displayMode == 5):
-				draw.rectangle((0,0,width,height), outline=0, fill=0)
-				draw.text((0, 0), copyrightTest, font=font, fill=255)
-				draw.text((0, 32), "All Rights Reserved", font=font, fill=255)
-				disp.image(image)
+		draw.rectangle((0,0,width,height), outline=0, fill=0)
+		draw.text((0, 0), copyrightText1, font=font, fill=255)
+		draw.text((0, 32), copyrightText2, font=font, fill=255)
+		disp.image(image)
 	
 	else:
-				disp.clear()
-				disp.display()
+		disp.clear()
+
+	disp.display()
 				
 def set2Lines(l1, l2):
 	global temp
@@ -99,20 +114,20 @@ def setIpAddress(l1):
 	Display()
 	
 def setImage(im):
-	global image
-	image=im
+	global imageFile
+	imageFile=im
 	Display()
 	
 def setDisplayMode(newMode):
-		global displayMode
-		displayMode = newMode
-		Display()
+	global displayMode
+	displayMode = newMode
+	Display()
 
 def incrementDisplayMode():
-		global displayMode
-		global validNodeCount
-		displayMode = (displayMode + 1) % validNodeCount
-		Display()
+	global displayMode
+	global validNodeCount
+	displayMode = (displayMode + 1) % validNodeCount
+	Display()
 		
 # MODES
 # 0 - temp and humidity
@@ -125,7 +140,7 @@ def incrementDisplayMode():
 setDisplayMode(0)
 set2Lines("21.1","47")
 time.sleep(2)
-set2Lines("and", "again")
+set2Lines("22", "99")
 time.sleep(2)
 setIpAddress("192.168.1.3")
 time.sleep(2)
@@ -168,4 +183,5 @@ time.sleep(0.5)
 
 disp.clear()
 disp.display()
+
 
